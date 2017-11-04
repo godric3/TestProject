ec85d30422d36ba73f6cf803eb7d5d78dd87f056
godric3 <zakandrzej3@gmail.com>
2017-11-04 15:03:38 +0100

    Database

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,40 @@
+import javax.ejb.EJB;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("/")
 public class HelloWorld {
 
+	
+	@EJB
+	TestEJB ejb;
+	
 	@GET
 	@Path("/")
 	public Response helloWorld() {
 		return Response.ok("Hello World!").build();
+	}
+	
+	@GET
+	@Path("/create")
+	public Response create() {
+		TestEntity testEntity=new TestEntity();
+		testEntity.setName("test");
+		ejb.persist(testEntity);
+		return Response.ok("created id: " + testEntity.getId() + " name: "+testEntity.getName()).build();
+	}
+	@GET
+	@Path("/show/{id}")
+	public Response show(@PathParam("id") int id) {
+		TestEntity testEntity=ejb.find(TestEntity.class, id);
+		if(testEntity == null)
+			return Response.status(Status.NOT_FOUND).build();
+		return Response.ok("found id: " + testEntity.getId() + " name: "+testEntity.getName()).build();
 	}
 
 }
---
+++ src/TestEJB.java
@@ -0,0 +1,18 @@
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class TestEJB {
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void persist(TestEntity testEntity) {
+		entityManager.persist(testEntity);
+		
+	}
+
+	public TestEntity find(Class<TestEntity> class1, int id) {
+		return entityManager.find(class1, id);
+	}
+}
