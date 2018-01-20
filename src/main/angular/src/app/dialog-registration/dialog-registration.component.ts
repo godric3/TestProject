8040e664561286c6de94e3c532d85a6c643aaccc
Stencil04 <czopera@gmail.com>
2018-01-21 00:07:36 +0100

    Separation components, add login & registration to dialogs, add ui-carousel

---
+++ src/main/angular/src/app/dialog-registration/dialog-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-registration',
+  templateUrl: './dialog-registration.component.html',
+  styleUrls: ['./dialog-registration.component.css']
+})
+export class DialogRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
