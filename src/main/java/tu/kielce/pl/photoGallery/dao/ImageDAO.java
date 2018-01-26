ec2add6096476c1336213e6e2a831824a5dfe58d
kingak <bighealthkick@gmail.com>
2018-01-26 14:38:23 +0100

    Change NotFoundException to EntityNotFound

--- src/main/java/tu/kielce/pl/photoGallery/dao/ImageDAO.java
+++ src/main/java/tu/kielce/pl/photoGallery/dao/ImageDAO.java
@@ -6,6 +6,7 @@ import javax.ejb.Stateless;
 import javax.persistence.Query;
 import javax.ws.rs.NotFoundException;
 
+import tu.kielce.pl.photoGallery.exception.EntityNotFound;
 import tu.kielce.pl.photoGallery.model.Category;
 import tu.kielce.pl.photoGallery.model.Image;
 import tu.kielce.pl.photoGallery.model.Tag;
@@ -37,28 +38,28 @@ public class ImageDAO extends GenericDAO<Image> {
 		return images;
 	}
 
-	public List<Image> getByExtension(String extension) throws NotFoundException {
+	public List<Image> getByExtension(String extension) throws EntityNotFound {
 		Query q = entityManager.createNamedQuery("Image.findByExtension", Image.class);
 		q.setParameter("extension", extension);
 		List<Image> images = q.getResultList();
 		return images;
 	}
 
-	public List<Image> getByTag(Tag tag) throws NotFoundException {
+	public List<Image> getByTag(Tag tag) throws EntityNotFound {
 		Query q = entityManager.createNamedQuery("Image.findByTag", Image.class);
 		q.setParameter("tag", tag);
 		List<Image> images = q.getResultList();
 		return images;
 	}
 
-	public List<Image> getByUser(User user) throws NotFoundException {
+	public List<Image> getByUser(User user) throws EntityNotFound {
 		Query q = entityManager.createNamedQuery("Image.findByUser", Image.class);
 		q.setParameter("user", user);
 		List<Image> images = q.getResultList();
 		return images;
 	}
 
-	public List<Image> getByCategory(Category category) throws NotFoundException {
+	public List<Image> getByCategory(Category category) throws EntityNotFound {
 		Query q = entityManager.createNamedQuery("Image.findByCategory", Image.class);
 		q.setParameter("category", category);
 		List<Image> images = q.getResultList();
