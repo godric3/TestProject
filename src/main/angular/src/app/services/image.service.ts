34a8566a0bd3b018407311b75b16f4372c4d7e07
Stencil04 <czopera@gmail.com>
2018-01-26 16:38:45 +0100

    add show one picture

--- src/main/angular/src/app/services/image.service.ts
+++ src/main/angular/src/app/services/image.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { Http, RequestOptions, Headers } from '@angular/http'
+import { Http, RequestOptions, Headers, URLSearchParams} from '@angular/http'
 import { UserService } from './user-service.service';
+import { ResponseContentType } from '@angular/http/src/enums';
+
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ImageService {
 
-  constructor(private http: Http,private userService:UserService) { }
+  constructor(private http: Http,private httpClient: HttpClient,private userService:UserService) { }
 
   sendImage(data: FormData) {
     let headers = new Headers({ 'Content_Type': 'multipart/form-data' });
@@ -13,5 +18,15 @@ export class ImageService {
     //headers.append('Authorization', 'token')
     let options = new RequestOptions({ headers: headers })
     return this.http.post('http://localhost:8080/TestProject/api/image', data, options)
+  }
+
+  getImage(id:string):Observable<any>{
+    //let headers = new Headers() 
+    //let myParams = new URLSearchParams()
+    //headers.append('Authorization',this.userService.getToken())
+    //myParams.append('id',id)
+    //let options = new RequestOptions({ headers: headers})
+    let headers:HttpHeaders = new HttpHeaders().set('Authorization', this.userService.getToken())
+    return this.httpClient.get('http://localhost:8080/TestProject/api/image/'+id,{headers:headers,responseType:'blob'})
   }
 }
--- src/main/angular/src/app/site/get-photo-page/get-photo-page.component.ts
+++ src/main/angular/src/app/site/get-photo-page/get-photo-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, } from '@angular/core';
+import { ImageService } from '../../services/image.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { SafeUrl } from '@angular/platform-browser/src/security/dom_sanitization_service';
 
 @Component({
   selector: 'app-get-photo-page',
@@ -8,20 +11,32 @@ import { Component, OnInit, ChangeDetectorRef, } from '@angular/core';
 export class GetPhotoPageComponent implements OnInit {
   selected: string
   filtrType: any
+  filtrType2: any
+  tags: string[]
+  imageContener: SafeUrl[];
   filtrs = [
     { value: 'filtr0', viewValue: 'Kategoria' },
     { value: 'filtr1', viewValue: 'Tagi' },
-    { value: 'filtr2', viewValue: 'Rozmiar' }
+    { value: 'filtr2', viewValue: 'Rozmiar' },
+    { value: 'filtr3', viewValue: 'Rozszerzenie' },
+    { value: 'filtr4', viewValue: 'Nazwa' }
   ];
 
-  constructor(private cdRef:ChangeDetectorRef) { }
+  constructor(private imageService: ImageService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+    this.imageContener = []
   }
 
-  showChange(){
+  addTagToTagsList() {
+    this.tags.push(this.filtrType);
+    this.filtrType = '';
+  }
+
+  showChange() {
     console.log('change')
-    this.filtrType = null 
+    this.filtrType = null
+    this.filtrType2 = null
   }
   checkSelected(value: string) {
     if (this.selected == value) {
@@ -31,4 +46,15 @@ export class GetPhotoPageComponent implements OnInit {
       return false
   }
 
+  searchImage() {
+    this.imageContener = []
+    this.imageService.getImage(this.filtrType).subscribe(res => {
+      const imageUrl = URL.createObjectURL(res);
+      this.imageContener.push(this.sanitizer.bypassSecurityTrustUrl(imageUrl))
+    })
+  }
+
+  getAllImages(){
+      
+  }
 }
